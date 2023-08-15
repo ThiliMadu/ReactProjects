07298d587f5e7df1f51452f17f405cedea94f45d
ThiliMadu <mangali.cst@gmail.com>
2023-08-15 16:26:49 -0400

    Updated files with latws chnages

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React123
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import './App.css';
+import { Header } from './components/Header'
+import { TaskLists } from './components/TaskLists';
+import { Footer } from './components/Footer';
+const info = 'Random';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React123
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <h1>Welcome</h1>
+      <TaskLists title="Random" subTitle='Test' info={info} />
+      <Footer />
     </div>
-  );
-}
+  )
 
+}
 export default App;
